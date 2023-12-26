28588c58085d0fca759c67d459abb8cfea07f587
gulab-coder-x <139480723+gulab-coder-x@users.noreply.github.com>
2023-12-26 12:08:18 +0530

    Update ProductToggleAttribute.php

--- ProductToggleAttribute.php
+++ ProductToggleAttribute.php
@@ -47,3 +47,56 @@ class InstallData implements InstallDataInterface
 	}
 	
 }
+
+
+<module name="Kinex_CustomModule" setup_version="1.2.0">
+<?php
+
+namespace Kinex\CustomModule\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    private $eavSetupFactory;
+
+	public function __construct(EavSetupFactory $eavSetupFactory)
+	{
+		$this->eavSetupFactory = $eavSetupFactory;
+	}
+
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+	{
+		if (version_compare($context->getVersion(), '1.2.0', '<')) {
+			$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+            $eavSetup->addAttribute(
+                \Magento\Catalog\Model\Product::ENTITY,'is_discontinued',
+                [
+                    'type' => 'int',
+                    'backend' => '',
+                    'frontend' => '',
+                    'label' => 'Is Discontinued',
+                    'input' => 'boolean',
+                    'class' => '',
+                    'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                    'visible' => true,
+                    'required' => false,
+                    'user_defined' => false,
+                    'default' => 0,
+                    'searchable' => false,
+                    'filterable' => false,
+                    'comparable' => false,
+                    'visible_on_front' => false,
+                    'used_in_product_listing' => true,
+                    'unique' => false,
+                    'apply_to' => ''
+                ]
+            );
+		}
+	}
+}
